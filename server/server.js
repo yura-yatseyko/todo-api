db277413768d9d629a2839eea068052cc9ce476f
Yatseyko Yuriy <yura.volpis@gmail.com>
2018-10-03 19:03:17 +0300

    Setup app for heroku.

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -33,8 +35,8 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log('Started on port ', port);
 });
 
 module.exports = {app};
